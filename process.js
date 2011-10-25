535d95303e8bf64da95ef3a0f0013fa6fb84e5d2
Callum Macrae <callum@lynxphp.com>
2011-10-25 11:06:24 +0100

    Fixed a couple more bugs and removed debugging stuff

--- process.js
+++ process.js
@@ -2,7 +2,7 @@ gen_mess = function(info) {
 	var loves = (info.loved) ? '<3 - ' : '';
 	return info.user + ' ' + ((info.current) ? 'is' : 'was') + ' listening to ' + info.artist
 		+ ' - ' + info.track + ' from the album ' + info.album + ' (' + loves + 'played '
-		+ info.plays + 'times)';
+		+ info.plays + ' times)';
 }
 
 message_parse = function(un, chan) {
@@ -26,16 +26,15 @@ message_parse = function(un, chan) {
 			try {
 				body = JSON.parse(body);
 				var t = body.recenttracks.track[0];
-				info.current = (t['@attr'] === undefined);
+				info.current = (t['@attr'] !== undefined);
 				info.user = body.recenttracks['@attr'].user;
 				info.artist = t.artist['#text'];
 				info.track = t.name;
-				info.album = t.album['#album'];
+				info.album = t.album['#text'];
 				
 				options.path = '/2.0/?method=track.getinfo&format=json&api_key='
 					+ config.api_key + '&artist=' + encodeURIComponent(info.artist)
-					+ '&track=' + encodeURIComponent(info.track) + '&username=' + info.user
-				console.log(options.path);
+					+ '&track=' + encodeURIComponent(info.track) + '&username=' + info.user;
 				var req2 = http.get(options, function(res) {
 					var body2 = '';
 					res.on('data', function(chunk) {
